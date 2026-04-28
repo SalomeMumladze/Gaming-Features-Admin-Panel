8bb404f29b53b84af119dd1f66cfb436afda0740
salomeMumladze <salomemumladze775@gmail.com>
2026-04-29 01:11:41 +0400

    fixed errors

--- src/shared/Drawers.tsx
+++ src/shared/Drawers.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import useQueryParams from "@/shared/providers/useQueryParams";
-import {LeaderboardEditDrawer} from '../features/leaderboards/drawers/LeaderboardEditDrawer';
-import {CreateLeaderboardDrawer} from '../features/leaderboards/drawers/CreateLeaderboardDrawer';
-import { RaffleEditDrawer } from "../features/Raffle/drawers/RaffleEditDrawer";
-import { CreateRaffleDrawer } from "../features/Raffle/drawers/CreateRaffleDrawer";
+// import {LeaderboardEditDrawer} from '../features/leaderboards/drawers/LeaderboardEditDrawer';
+// import {CreateLeaderboardDrawer} from '../features/leaderboards/drawers/CreateLeaderboardDrawer';
+// import { RaffleEditDrawer } from "../features/Raffle/drawers/RaffleEditDrawer";
+// import { CreateRaffleDrawer } from "../features/Raffle/drawers/CreateRaffleDrawer";
 // import {
 //   RaffleEditDrawer,
 //   CreateRaffleDrawer,
 // } from "@/features/Raffle/drawers";
-import { CreateWheelDrawer } from "../features/Wheel/drawers/CreateWheelDrawer";
-import { EditWheelDrawer } from "../features/Wheel/drawers/EditWheelDrawer";
+// import { CreateWheelDrawer } from "../features/Wheel/drawers/CreateWheelDrawer";
+// import { EditWheelDrawer } from "../features/Wheel/drawers/EditWheelDrawer";
 // import { CreateWheelDrawer, EditWheelDrawer } from "@/features/Wheel/drawers";
 
 interface DrawerProps {
@@ -27,12 +27,12 @@ const Drawers: React.FC = () => {
   const params = useQueryParams();
 
   const drawerComponents = [
-    LeaderboardEditDrawer,
-    CreateLeaderboardDrawer,
-    RaffleEditDrawer,
-    CreateRaffleDrawer,
-    CreateWheelDrawer,
-    EditWheelDrawer,
+    // LeaderboardEditDrawer,
+    // CreateLeaderboardDrawer,
+    // RaffleEditDrawer,
+    // CreateRaffleDrawer,
+    // CreateWheelDrawer,
+    // EditWheelDrawer,
   ] as DrawerComponentType[];
 
   return (
