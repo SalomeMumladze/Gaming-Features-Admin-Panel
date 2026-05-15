0bd4d7f73cb8a4aa78de3779aef66859e679e2b1
salomeMumladze <salomemumladze775@gmail.com>
2026-05-15 18:47:27 +0400

    add env

--- src/shared/api/httpClient.ts
+++ src/shared/api/httpClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiGateway = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
 });
